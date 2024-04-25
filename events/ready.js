b3729011822096e2485669c3a2c6f1b7da2033fa
usmansss <159324104+usmansss@users.noreply.github.com>
2024-04-25 21:13:57 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const { EmbedBuilder } = require("discord.js")
+const db = require("croxydb")
+
+module.exports = {
+  name: Discord.Events.ClientReady,
+
+  run: async(client, message) => {
+    console.log(`${client.user.tag} Aktif!`);
+    const activities = [
+      "❤️ | SA-SHOP ❤️",
+      "👮🏽‍♂️ | By SilentA",
+
+    ]
+    
+    setInterval(async() => {
+      client.user.setPresence({ activities: [{ name: `${activities[Math.floor(Math.random() * activities.length)]}` }], status: 'idle' });
+    }, 1000 * 15);
+	db.set(`botAcilis_`, Date.now())
+
+
+  }
+}
